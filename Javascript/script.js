2330e136b02308a32fbfacfbf2d430c594fa36a1
Svartingknas <svartingknas@gmail.com>
2016-10-12 22:23:20 -0600

    func(init) added func/ fixed html

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,1 +1,22 @@
 "use strict"
+
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract (a, b) {
+  return a - b;
+}
+
+function mult(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function remainder(a, b) {
+  return a % b;
+}
