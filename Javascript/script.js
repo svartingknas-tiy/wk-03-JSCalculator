6c0ec83f941f720d7831578236b2ec8561b01f9a
Svartingknas <svartingknas@gmail.com>
2016-12-20 07:31:20 -0700

    add functions for calculations

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,13 +1,33 @@
 alert("welcome to calculator");
 
 function AddDigit(value) {
-  document.getElementById("Display").value += value;
+  document.getElementById("display").value += value;
 }
 
-function result(){
-  document.getElementById("Display").value = eval(document.getElementById("Display").value);
-}
+// function result(){
+//   document.getElementById("Display").value = eval(document.getElementById("Display").value);
+// }
 
 function reset(){
   document.getElementById("Display").value = " ";
+}
+
+function plus(a, b){
+  return Number(a) + Number(b);
+}
+
+function sub(a, b){
+  return Number(a) - Number(b);
+}
+
+function mult(a, b){
+  return Number(a) * Number(b);;
+}
+
+function div(a, b){
+  return Number(a) / Number(b);
+}
+
+function mod(a, b){
+  return Number(a) % Number(b);
 }
