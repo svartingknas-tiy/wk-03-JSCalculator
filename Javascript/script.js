b6e23ab9dcf55cdbb3ba36c41a496966ab89dea2
Svartingknas <svartingknas@gmail.com>
2017-08-06 19:08:45 -0600

    create gitignore

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,22 +1,25 @@
 alert("welcome to calculator");
 
+var calcInput = [];
+var inputString = "";
+
 function AddDigit(value) {
   document.getElementById("display").value += value;
 }
 
 // function result(){
-//   document.getElementById("Display").value = eval(document.getElementById("Display").value);
+//   document.getElementById("display").value = eval(document.getElementById("display").value);
 // }
 
 function reset(){
-  document.getElementById("Display").value = " ";
+  document.getElementById("display").value = " ";
 }
 
 function plus(a, b){
   return Number(a) + Number(b);
 }
 
-function sub(a, b){
+function subs(a, b){
   return Number(a) - Number(b);
 }
 
